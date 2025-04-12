2e8b74fe9b2a398f2467f75271d35aa48544de88
Nikita Sherbakov <nikitkaoiama@gmail.com>
2025-04-12 22:16:01 +0300

    testLogin

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -34,4 +34,59 @@ public class LoginTest {
         driver.quit();
     }
 
+    @Test
+    public void testLogin2() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        String masssage = driver.findElement(By.cssSelector("[data-test=error]")).getText();
+
+        Assert.assertEquals(masssage, "Epic sadface: Username is required");
+        driver.quit();
+    }
+
+    @Test
+    public void testLogin3() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("1234");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        String masssage = driver.findElement(By.cssSelector("[data-test=error]")).getText();
+
+        Assert.assertEquals(masssage, "Epic sadface: Username and password do not match any user in this service");
+        driver.quit();
+    }
+
+    @Test
+    public void testLogin4() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("12345");
+        driver.findElement(By.id("password")).sendKeys("123456789");
+        driver.findElement(By.id("login-button")).click();
+
+        String masssage = driver.findElement(By.cssSelector("[data-test=error]")).getText();
+
+        Assert.assertEquals(masssage, "Epic sadface: Username and password do not match any user in this service");
+        driver.quit();
+    }
+    @Test
+    public void testLogin5() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("");
+        driver.findElement(By.id("password")).sendKeys("");
+        driver.findElement(By.id("login-button")).click();
+
+        String masssage = driver.findElement(By.cssSelector("[data-test=error]")).getText();
+
+        Assert.assertEquals(masssage, "Epic sadface: Username is required");
+        driver.quit();
+    }
+
 }
